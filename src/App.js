9d5607dd5f310b3ba2f8e2344753ff53b015fb24
Angeluz99 <122189395+Angeluz99@users.noreply.github.com>
2024-07-24 20:05:41 -0600

    smdigitalApp.js

--- src/App.js
+++ src/App.js
@@ -74,13 +74,12 @@ const App = () => {
       
       <div id='head-content'>
         <p >
-        Somos<strong> distribuidores de medicina regenerativa</strong> con un equipo altamente capacitado y <strong>más de 10 años de experiencia en terapia celular.</strong> Estamos aquí para brindarte el <strong> apoyo necesario en el tratamiento de tus pacientes.</strong>
-        </p>
-        <img id='head-image' src={process.env.PUBLIC_URL + '/images/headimage.png'} alt='pharmaceutical'  loading="lazy"/>
-        <img id='head-image2' src={process.env.PUBLIC_URL + '/images/headimage2.png'} alt='pharmaceutical'  loading="lazy"/>
+        Dedicados al manejo de <strong>redes sociales</strong>, creación de contenido diseño y rediseño de logotipo, así como <strong>diseño e impresión de material gráfico</strong> para pequeños negocios. </p>
+        <img id='head-image' src={process.env.PUBLIC_URL + '/images/smlogo.png'} alt='smlogo'  loading="lazy"/>
+        {/* <img id='head-image2' src={process.env.PUBLIC_URL + '/images/headimage2.png'} alt='pharmaceutical'  loading="lazy"/> */}
       </div>
 
-        <div className='social-head d-flex justify-content-end'>
+      <div className='social-head d-flex justify-content-end'>
         {/* <div className='social-head-child d-flex justify-content-around border border-success' > */}
         <a className="whatsAppHead" href="https://wa.me/523345165088/?text=Quisiera%20saber%20más%20de%20sus%20productos" target="_blank" rel="noopener noreferrer">
           <i className="bi bi-whatsapp"></i>
